8e4f71b5365d138750f6e5380ab98690b3301ae1
shifra-fraiman <s0548549680@gmail.com>
2024-12-01 21:18:35 +0200

    Create a creteBussiness and home pages and style and more..

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import { Home } from './pages/home'
+import { Home } from './pages/HomePage/HomePage'
 
 function App() {
  
