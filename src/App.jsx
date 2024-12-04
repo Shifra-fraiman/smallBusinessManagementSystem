f2bd451cd6435fa2cf062456076b3d36ab655e34
Chaya Krashinski <>
2024-12-04 12:20:25 +0200

    create header

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@
 // import viteLogo from '/vite.svg'
 import './App.css'
 import { Home } from './pages/HomePage/HomePage.page'
+// import { ResponsiveAppBar } from './components/Header/Header.component'
+import ResponsiveAppBar from './components/Header/Header.component';
+
 
 function App() {
  
 
   return (
     <>
+    <ResponsiveAppBar></ResponsiveAppBar>
     <Home></Home>
     </>
   )
