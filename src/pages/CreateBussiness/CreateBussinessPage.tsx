378e3394fd771f83953d94812440fde95cee82aa
shifra-fraiman <s0548549680@gmail.com>
2024-12-17 19:46:28 +0200

    Use stepper and forms

---
+++ src/pages/CreateBussiness/CreateBussinessPage.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { UserForm } from "../../components/Forms/UserForm/UserForm.component";
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+import "./CreateBussinessPage.scss";
+import { CreateStepper } from "../../components/general/CreateStepper";
+import { BussinessForm } from "../../components/Forms/BusinessForm/BussinesForm";
+import { ServiceForm } from "../../components/Forms/ServiceForm/ServiceForm";
+import { ChooseDayInWeekAndTime } from "../../components/general/ChooseDayInWeekAndTime";
+
+
+
+
+export const CreateBussinessPage = ()=>{
+    // function valuetext(value: number) {
+    //     return `${value}°C`;
+    //   }
+
+const [currentForm, setCurrentForm]= useState(0);
+
+    const NextForm=()=>{
+        console.log("NextFunc");
+        
+        setCurrentForm(currentForm+1);
+    }
+
+    return (
+        <>
+        {/* <div className="slider">
+        <Box sx={{ width: 500 }}>
+          <Slider 
+  aria-label="Temperature"
+  defaultValue={40}
+  getAriaValueText={valuetext}
+  color="secondary"
+  
+/>
+    </Box>
+            
+        </div> */}
+      
+        <CreateStepper key = {currentForm} value={currentForm}></CreateStepper>  
+           
+        {currentForm === 0 && <UserForm onNext={NextForm} />}
+        {currentForm === 1 && <BussinessForm onNext={NextForm} />}
+        {currentForm === 2 && <ServiceForm onNext={NextForm} />}
+
+        </>
+    )
+}
