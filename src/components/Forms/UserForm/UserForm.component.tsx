d0a99347cc4be762d2e1571020b3491eb0e3b93f
shifra-fraiman <s0548549680@gmail.com>
2024-12-17 20:20:22 +0200

    user form validation

--- src/components/Forms/UserForm/UserForm.component.tsx
+++ src/components/Forms/UserForm/UserForm.component.tsx
@@ -14,6 +14,7 @@ import {
 } from "@mui/material";
 import {Password, Visibility} from "@mui/icons-material";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import FormHelperText from '@mui/material/FormHelperText';
 import "./UserForm.css";
 
 export const UserForm = ({onNext}) => {
@@ -22,7 +23,12 @@ export const UserForm = ({onNext}) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
-        Password: ''
+        password: ''
+    });
+    const [errors, setErrors] = useState({
+        name: '',
+        email: '',
+        password: ''
     });
 
     const handleChange = (event) => {
@@ -31,6 +37,10 @@ export const UserForm = ({onNext}) => {
         setFormData({
             ...formData,
             [name]: value
+        });
+        setErrors({
+            ...errors,
+            [name]: ''
         });
        
     };
@@ -44,13 +54,38 @@ export const UserForm = ({onNext}) => {
     // const handleMouseUpPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
     //     event.preventDefault();
     // };
-    
+    const validateForm = () => {
+        let valid = true;
+        let newErrors = { name: '', email: '', password: '' };
+
+        if (formData.name.trim() === '') {
+            newErrors.name = 'שדה זה הינו חובה';
+            valid = false;
+        }
+        if (formData.email.trim() === '') {
+            newErrors.email = 'שדה זה הינו חובה';
+            valid = false;
+        } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+            newErrors.email = 'מייל לא תקין';
+            valid = false;
+        }
+        if (formData.password.trim() === '') {
+            newErrors.password = 'שדה זה הינו חובה';
+            valid = false;
+        }
+
+        setErrors(newErrors);
+        return valid;
+    };
 
     const submitUserForm = (event) => {
         event.preventDefault();
-        alert(`${formData.Password},${formData.email}, ${formData.name}`);
-        //שליחה לסרוויס 
-        onNext();
+        if(validateForm()){
+            alert(`${formData.password},${formData.email}, ${formData.name}`);
+            //שליחה לסרוויס 
+            onNext();
+        }
+      
     };
 
     return ( 
@@ -60,9 +95,12 @@ export const UserForm = ({onNext}) => {
                 variant="outlined"
                 margin="normal"
                 fullWidth
+                
             >
                 <InputLabel htmlFor="name">שם</InputLabel>
-                <OutlinedInput id="name" label="שם" name="name" onChange={handleChange}/>
+                <OutlinedInput id="name" label="שם" name="name" onChange={handleChange} error={!!errors.name}/>
+                {errors.name && <FormHelperText error>{errors.name}</FormHelperText>}
+
             </FormControl>
             <FormControl
                 sx={{m: 1, mb: 2, width: "50ch", textAlign: "right"}}
@@ -71,7 +109,9 @@ export const UserForm = ({onNext}) => {
                 fullWidth
             >
                 <InputLabel htmlFor="email" >מייל</InputLabel>
-                <OutlinedInput id="email" label="מייל" type="email" name="email" onChange={handleChange} />
+                <OutlinedInput id="email" label="מייל" type="text" name="email" onChange={handleChange} error={!!errors.email}/>
+                {errors.email && <FormHelperText error>{errors.email}</FormHelperText>}
+
             </FormControl>
             <FormControl sx={{m: 1, mb: 2, width: "50ch"}} variant="outlined" margin="normal" fullWidth>
                 <InputLabel htmlFor="password">סיסמה</InputLabel>
@@ -80,6 +120,7 @@ export const UserForm = ({onNext}) => {
                     type={showPassword ? "text" : "password"}
                     onChange={handleChange}
                     name="password"
+                    error={!!errors.password}
                     endAdornment={
                         <InputAdornment position="end"  >
                             <IconButton
@@ -95,6 +136,7 @@ export const UserForm = ({onNext}) => {
                     }
                     label="Password"
                 />
+                   {errors.password && <FormHelperText error>{errors.password}</FormHelperText>}
             </FormControl>
             <Button
                 sx={{m: 1, mb: 2, width: "25ch", color: "linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)", background: "linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)"}}
